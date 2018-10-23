3c4642eb1ac2a40d95e08a1b3406fe05b6e30355
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2018-10-23 13:05:28 +0300

    [ch-#161382378] Ugrade Django version from 1.10 to 2.1

--- authors/urls.py
+++ authors/urls.py
@@ -1,7 +1,7 @@
 """authors URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-
-    url(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
+    path('admin/', admin.site.urls),
+    path('api/', include(('authors.apps.authentication.urls', 'authors.apps.authentication'),
+                         namespace='authentication')),
 ]
 
