5fdd928fa89c214bcc2a3819fc22274835bbd41d
cdvx <cdvx@fox.com>
2018-10-24 09:44:46 +0300

    [chore] Fetch updates from develop

--- authors/settings.py
+++ authors/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ah_backend', # os.path.join(BASE_DIR, 'Users/cdvx/Desktop/django-unchaned/tictactoe1'),
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -138,6 +142,6 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authors.apps.authentication.backends.JWTAuthentication',
+        # 'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
